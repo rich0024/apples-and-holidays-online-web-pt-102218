5d5efbd17f34f067f14333170a496619b7a22d06
Richard Yance <yance0022@gmail.com>
2018-11-05 05:58:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -73,6 +73,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
       if supplies.includes? "bbq"
-        returns holiday 
+        returns holiday
   end
 end
