6dfcdebe4b32b7db15031e1b87931fd9f9aefd61
Richard Yance <yance0022@gmail.com>
2018-11-05 06:11:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,7 +74,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
       if supplies == "BBQ"
-    holiday
+        holiday
     arr << holiday
   end
 end
