bbc36665bfe7d1209b00e3a53e6dfb4aa90c958c
Richard Yance <yance0022@gmail.com>
2018-11-05 05:22:37 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, data|
   data.each do |holiday, supplies|
     puts "#{season.capitalize}:"
-    puts "  #{holiday.capitalize}: #{supplies[0]}, #{supplies[1]}"
+    puts "  #{holiday.capitalize.sub(_,  )}: #{supplies}"
   end
 end
 end
